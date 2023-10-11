942c94c4259e8f6b41f4c9505b4d7ecd04f4dc44
golubtsovladimir <97600165+golubtsovladimir@users.noreply.github.com>
2023-10-11 20:46:14 +0400

    Add Card Images

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Item(props){
   return(
     <div className="item">
-      <img src={""}/>
+      <img src={"./img/"+ props.item2.img} width={"300px"}/>
       <h2>{props.item2.title}</h2>
       <p>{props.item2.desc}</p>
       <b>{props.item2.price}</b>
