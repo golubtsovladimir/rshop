ebb98f8386480e71992ad97a58358a3a5ceb8288
golubtsovladimir <97600165+golubtsovladimir@users.noreply.github.com>
2023-10-25 19:54:03 +0400

    full-item and json-server

--- src/components/ShowFullItem.jsx
+++ src/components/ShowFullItem.jsx
@@ -2,7 +2,14 @@ import React from "react";
 
 export default function ShowFullItem(props){
   return(
-    <>
-    </>
+    <div className="full-item">
+      <div>
+        <img src={"./img/"+ props.item.img} width={"300px"} onClick={()=>props.onShowItem(props.item)}/>
+        <h2>{props.item.title}</h2>
+        <p>{props.item.desc}</p>
+        <b>{props.item.price}</b>
+        <div className="add-to-cart" onClick={()=>props.onAdd(props.item)}>+</div>
+      </div>
+    </div>
   );
 }
